b448bbbb3cdd9a5e9027000a49c77f80e9d7d298
TJ Eastmond <tj.eastmond@gmail.com>
2025-02-06 17:47:20 -0500

    Wire it up

--- main.go
+++ main.go
@@ -1,60 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tjeastmond/future-take-home/routes"
+	"github.com/tjeastmond/future-take-home/store"
 )
 
-type Appointment struct {
-	ID        int    `json:"id"`
-	TrainerID int    `json:"trainer_id"`
-	ClientId  int    `json:"user_id"`
-	StartsAt  string `json:"started_at"`
-	EndsAt    string `json:"ended_at"`
-}
-
-func parseAppointmentsFile(filename string) (error, []Appointment) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err, nil
-	}
-	defer file.Close()
-
-	var appointments []Appointment
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&appointments)
-	return err, appointments
-}
-
-func listAppointments(c *gin.Context) {
-	file, err := os.Open("docs/appointments.json")
-	if err != nil {
-		log.Fatalf("failed to open file: %v", err)
-	}
-
-	defer file.Close()
-
-	err, appointments := parseAppointmentsFile("docs/appointments.json")
-
-	c.JSON(http.StatusOK, gin.H{
-		"data": appointments,
-	})
-}
-
 func main() {
+	if err := store.LoadData(); err != nil {
+		log.Printf("Warning: Failed to load data from file: %v", err)
+	}
+
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, world!",
-		})
-	})
+	routes.InitializeRoutes(router)
 
-	router.GET("/list", listAppointments)
-
-	router.Run(":8080")
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
